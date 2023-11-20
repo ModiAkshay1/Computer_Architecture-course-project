3d12539f9c4df976e1178fb68d0802a284388075
ModiAkshay1 <99759224+ModiAkshay1@users.noreply.github.com>
2023-11-20 14:38:01 +0530

    Add files via upload

--- delay.py
+++ delay.py
@@ -1,3 +1,5 @@
+import random
+
 delay_int = open("delay.txt","r")
 delay_int = delay_int.read()
 delay_max=0
@@ -19,4 +21,20 @@ for i in x:
     if (int(i) > delay_max):
         delay_max = int(i)
 
+def generate_random_value(mean):
+    std_dev=0.1*float(mean)
+    while True:
+        random_val = random.gauss(mean, std_dev)
+        if mean - 3 * std_dev <= random_val <= mean + 3 * std_dev:
+            return random_val
 
+all_delays=[]
+for i in range(9):
+    temp1=[]
+    for j in range(3):
+        temp1.append(generate_random_value(int(x[j])))
+    all_delays.append(temp1)
+
+for i in range(len(x)):
+    x[i]=int(x[i])
+print(all_delays)
